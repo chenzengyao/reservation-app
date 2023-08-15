6d96d0d7f0021b5a4baa9187eaa4be2a1bc287e9
MrDOUFY <jasontch0804@gmail.com>
2023-08-15 20:41:28 +0800

    Create Application

---
+++ src/main/java/com/tablehop/tablehop_restaurant_app/TablehopRestaurantAppApplication.java
@@ -0,0 +1,13 @@
+package com.tablehop.tablehop_restaurant_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablehopRestaurantAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TablehopRestaurantAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tablehop/tablehop_restaurant_app/TablehopRestaurantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tablehop.tablehop_restaurant_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TablehopRestaurantAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
