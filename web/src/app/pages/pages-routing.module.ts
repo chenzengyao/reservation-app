1c1768bbe45bc489f86794127754c1b8175db9d4
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2023-09-01 23:27:42 +0800

    update: sidebar menu & topbar

--- web/src/app/pages/pages-routing.module.ts
+++ web/src/app/pages/pages-routing.module.ts
@@ -4,9 +4,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { DefaultComponent } from './dashboards/default/default.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'dashboard' },
-  { path: 'dashboard', component: DefaultComponent },
-  { path: 'dashboards', loadChildren: () => import('./dashboards/dashboards.module').then(m => m.DashboardsModule) },
+  { path: '', redirectTo: 'admin/dashboard' },
+  { path: 'admin/dashboard', component: DefaultComponent },
+  { path: 'admin/dashboard', loadChildren: () => import('./dashboards/dashboards.module').then(m => m.DashboardsModule) },
 ];
 
 @NgModule({
