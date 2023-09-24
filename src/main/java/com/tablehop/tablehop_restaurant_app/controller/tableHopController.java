438633fbd88da7a57eaee88585d5023900c52f01
Jason <jasontoh123460@gmail.com>
2023-09-25 01:16:02 +0800

    Controller and service

--- src/main/java/com/tablehop/tablehop_restaurant_app/controller/tableHopController.java
+++ src/main/java/com/tablehop/tablehop_restaurant_app/controller/tableHopController.java
@@ -2,8 +2,11 @@ package com.tablehop.tablehop_restaurant_app.controller;
 
 import java.util.*;
 import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tablehop.tablehop_restaurant_app.service.tableHopService;
 
 
 @RestController
@@ -11,5 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class tableHopController {
     private static final Logger log = LoggerFactory.getLogger(tableHopController.class);
 
-
+    @Autowired
+    public tableHopService tableHopService;
 }
---
+++ src/main/java/com/tablehop/tablehop_restaurant_app/service/tableHopService.java
@@ -0,0 +1,8 @@
+package com.tablehop.tablehop_restaurant_app.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class tableHopService {
+
+}
