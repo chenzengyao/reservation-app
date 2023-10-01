4cfd28aad4eef61137beafd2c79e11beffd45395
Isaac Tan <iztan98@gmail.com>
2023-10-01 13:40:30 +0800

    update: order, delivery, user, tables listing new: add order, delivery, users, table  component

---
+++ web/src/app/pages/dashboards/delivery/add-delivery/add-delivery.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-delivery',
+  templateUrl: './add-delivery.component.html',
+  styleUrls: ['./add-delivery.component.scss']
+})
+export class AddDeliveryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ web/src/app/pages/dashboards/orders/add-orders/add-orders.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-orders',
+  templateUrl: './add-orders.component.html',
+  styleUrls: ['./add-orders.component.scss']
+})
+export class AddOrdersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
