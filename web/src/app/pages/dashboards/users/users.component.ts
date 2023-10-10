02febcfd6ba1046d7a47942a42def60cbf329c2c
Lizyee1120 <lizyee3174@gmail.com>
2023-10-11 01:50:14 +0800

    Update: UI enhancement

--- web/src/app/pages/dashboards/users/users.component.ts
+++ web/src/app/pages/dashboards/users/users.component.ts
@@ -37,6 +37,7 @@ export class UsersComponent implements OnInit {
     this.userProfileService.getAllUser().subscribe(data => {
       this.users = data.body as User[];
       console.log(this.users);
+      location.reload();
     })
   }
 
